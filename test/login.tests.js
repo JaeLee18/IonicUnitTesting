9a30c8d71305ddfa4abfe629a22818d6738b9ccb
JaeLee18 <lee2161@purdue.edu>
2016-03-19 20:08:18 -0400

    Added a fail test case

--- test/login.tests.js
+++ test/login.tests.js
@@ -1,4 +1,4 @@
-describe('Clicking on the login button ', function(){  
+describe('Testing loginCtrl.js ', function(){  
     var username, password, loginButton;
     
     beforeEach(function() {
@@ -19,16 +19,15 @@ describe('Clicking on the login button ', function(){
         });
     });
 
-//    it('should display a popup for an unsuccessful login', function() {
-//        // TODO: test unsuccessful login
-//            username.sendKeys('gonehybrid');
-//            password.sendKeys('idontknow');
-//        
-//    loginButton.click().then(function() {
-//        expect(browser.getLocationAbsUrl()).toMatch('/login');
-//
-//        var popup = element(by.css('.popup-container.popup-showing.active'));
-//        expect(popup.isDisplayed()).toBeTruthy();
-//    });
-//    });
+    
+    it('Login Failed', function() {
+        // TODO: test unsuccessful login
+        username.sendKeys('test3@test3.com');
+        password.sendKeys(12345);
+        loginButton.click().then(function() {
+        browser.sleep(1000);
+   
+        expect(browser.getLocationAbsUrl()).toMatch('/login');
+        });
+    });
 });
